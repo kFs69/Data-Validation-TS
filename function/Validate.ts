e112e9ee48304c031d53933c970a4e9d028e7ec1
Gustavo M. Paes <gump.flash@gmail.com>
2020-10-02 09:45:40 -0300

    TS validate error on vscode

--- function/Validate.ts
+++ function/Validate.ts
@@ -3,8 +3,8 @@ interface ValidationOptions {
   required: Boolean;
   type: any;
   identify?: string;
-  min?: number;
-  max?: number;
+  min: number;
+  max: number;
 }
 
 interface Objects {
@@ -19,32 +19,31 @@ interface ValidationReturn {
 function Validate(data: Objects): ValidationReturn {
   let error: Object[] = [];
 
-  Object.keys(data).map((item, index) => {
-    if(data[item].required === true && (data[item].value === null || data[item].value === undefined || data[item].value.length === 0)) {
-      error.push({ name: item, code: 1, msg: 'Value is required' });
-    } else if(data[item].identify && data[item].identify === 'email') {
-      var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      var isEmail = regex.test(String(data[item].value).toLowerCase());
-      if(!isEmail) {
-        error.push({ name: item, code: 2, msg: `Email not valid for ${item}` });
-      }
-    } else if(data[item].min && data[item].value.toString().length < data[item].min) {
-      error.push({ name: item, code: 3, msg: `Min length is ${data[item].min} for ${item}` });
-    } else if(data[item].max && data[item].value.toString().length > data[item].max) {
-      error.push({ name: item, code: 3, msg: `Max length is ${data[item].max} for ${item}` });
-    } else if(data[item].type && typeof(data[item].type()) === 'number') {
-      var regex = /^\d+$/;
-      var isNumber = regex.test(data[item].value);
-      if(!isNumber) {
-        error.push({ name: item, code: 4, msg: `Invalid data type ${data[item].value} for ${item}` });
-      }
-    }
-  })
+    Object.keys(data).map((item, index) => {
+        if(data[item].required === true && (data[item].value === null || data[item].value === undefined || data[item].value.length === 0)) 
+            error.push({ name: item, code: 1, msg: 'Value is required' });
+        else if(data[item].identify && data[item].identify === 'email') {
+            var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            var isEmail = regex.test(String(data[item].value).toLowerCase());
+            if(!isEmail) 
+                error.push({ name: item, code: 2, msg: `Email not valid for ${item}` });
+        }
+        else if(data[item].value.toString().length < data[item].min) 
+            error.push({ name: item, code: 3, msg: `Min length is ${data[item].min} for ${item}` });
+        else if(data[item].value.toString().length > data[item].max) 
+            error.push({ name: item, code: 3, msg: `Max length is ${data[item].max} for ${item}` });
+        else if(data[item].type && typeof(data[item].type()) === 'number'){
+            var regex = /^\d+$/;
+            var isNumber = regex.test(data[item].value);
+            if(!isNumber) 
+                error.push({ name: item, code: 4, msg: `Invalid data type ${data[item].value} for ${item}` });
+        }
+    })
 
-  return {
-    isValid: error.length > 0 ? false : true,
-    items: error
-  }
+    return {
+        isValid: error.length > 0 ? false : true,
+        items: error
+    }
 }
 
 export default Validate;
